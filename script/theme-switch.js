c754e42e90edf0c9870a58122c9aaad418c58071
Dima <Uchiha132132qqq@gmail.com>
2022-06-03 01:23:42 +0300

    add media for 1024px

--- script/theme-switch.js
+++ script/theme-switch.js
@@ -4,11 +4,15 @@ const highwayButton = document.querySelectorAll('.highway__button');
 const nightIcon = document.querySelector('.footer__theme-switch');
 const dayIcon = document.querySelector('.footer__theme');
 const footerEmail = document.querySelector('.footer__email');
-
+const bikesImageBorder = document.querySelectorAll('.bikes__border')
 buttonSwitch.addEventListener('click', () => {
   body.classList.toggle('body_night')
+
   highwayButton.forEach((el) => {
     el.classList.toggle('highway__button_night')
+  })
+  bikesImageBorder.forEach((el) => {
+    el.classList.toggle('bikes__border_night')
   })
   nightIcon.classList.toggle('footer__theme-switch_nigth')
   dayIcon.classList.toggle('footer__theme_night')
